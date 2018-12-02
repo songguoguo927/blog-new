110baeb2b85e11f2f83cf2b252b019e1e58308a8
songguoguo927 <1351443467@qq.com>
2018-12-02 21:35:07 +0800

    获取数据成功

--- routers/api.js
+++ routers/api.js
@@ -3,6 +3,6 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/user/register',function(req,res,next){
-    console.log('register');
+    console.log( req.body );
 });
 module.exports = router;
