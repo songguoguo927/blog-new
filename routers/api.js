4fde1369e1fe21a3a58d01a4bbe4e66aa35a939a
songguoguo927 <1351443467@qq.com>
2018-12-02 21:15:20 +0800

    用户注册路由

--- routers/api.js
+++ routers/api.js
@@ -3,7 +3,6 @@ var express = require('express');
 var router = express.Router();
 
 router.post('/user/register',function(req,res,next){
-
     console.log('register');
 });
 module.exports = router;
